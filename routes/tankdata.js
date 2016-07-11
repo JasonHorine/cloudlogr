6bd04cd0904c0af54cc46f969dfe01b3d53c9e02
Jason Horine <outbackdog@gmail.com>
2016-07-11 15:56:43 -0500

    Added Moment.js to format the date consistently from the server and browser.  Added a missing ID to keep the chart width constant.

--- routes/tankdata.js
+++ routes/tankdata.js
@@ -7,11 +7,13 @@ var Schedule = require('../models/schedule');
 var request = require('request');
 var parse = require('csv-parse');
 var babyparse = require('babyparse');
+var moment = require('moment');
+
 
 router.get('/', function(req, res, next) {
   Schedule.findOne( { user: 'Jason' }, function(err, schedule){ // get entry with user: Jason from DB
     if (schedule) {
-      res.render('tank', { schedule: schedule });
+      res.render('tank', { schedule: schedule, moment: moment });
     } else { response.send('could not find user: Jason.  Error: ' + err);
       // response.send('the schedule is: ' + schedule); works
     };
