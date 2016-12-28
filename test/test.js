da8940a1c24d25e051559f93db83c7c502fb7a5c
Jason Horine <outbackdog@gmail.com>
2016-12-27 21:35:59 -0600

    some unit testing is working

---
+++ test/test.js
@@ -0,0 +1,48 @@
+var assert = require('assert');
+var request = require('request');
+/* example code
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
+*/
+
+describe('API V2 tests:', function(){
+  describe('Start polling', function(){
+    it('Returns an object with polling: true.');
+  }),
+  describe('Stop polling', function(){
+    it('Returns an object with polling: false.');
+  }),
+  describe('Change poll rate.', function(){
+    it('Returns an object with pollRate at new rate.');
+  }),
+  describe('Get one reading', function(){
+    it('Returns database in body, wihtout errors.', function(done){
+      var apiResponse = {};
+      request('http://localhost:3000/api/v2/oneReading', function(error, response, body) {
+        apiResponse.error = error;
+        apiResponse.response = response;
+        apiResponse.body = body;
+
+        assert(apiResponse.body && !error, 'Missing body or recieved an error response.');
+        done();
+      })
+    });
+  }),
+  describe('Get all readings (.get /api/v2/data)', function(){
+    it('Returns database in body, without errors.', function(done){
+      var apiResponse = {};
+      request('http://localhost:3000/api/v2/data', function(error, response, body) {
+        apiResponse.error = error;
+        apiResponse.response = response;
+        apiResponse.body = body;
+        assert(apiResponse.body && !error, 'Missing body or recieved an error response.');
+        done();
+      })
+    })
+  })
+})
