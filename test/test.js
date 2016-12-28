06fbebaff255814615a33641fa8a82ded2573e4c
Jason Horine <outbackdog@gmail.com>
2016-12-28 08:19:45 -0600

    minor fix

--- test/test.js
+++ test/test.js
@@ -1,3 +1,6 @@
+//----------------------------------------------//
+//    'NPM test' from the command line to run
+//----------------------------------------------//
 var assert = require('assert');
 var request = require('request');
 /* example code
@@ -20,27 +23,18 @@ describe('API V2 tests:', function(){
   describe('Change poll rate.', function(){
     it('Returns an object with pollRate at new rate.');
   }),
-  describe('Get one reading', function(){
-    it('Returns database in body, wihtout errors.', function(done){
-      var apiResponse = {};
+  describe('Get one reading (.get /api/v2/oneReading)', function(){
+    it('Returns database in body, without errors.', function(done){
       request('http://localhost:3000/api/v2/oneReading', function(error, response, body) {
-        apiResponse.error = error;
-        apiResponse.response = response;
-        apiResponse.body = body;
-
-        assert(apiResponse.body && !error, 'Missing body or recieved an error response.');
+        assert(body && !error, 'Missing body or recieved an error response.');
         done();
       })
     });
   }),
   describe('Get all readings (.get /api/v2/data)', function(){
     it('Returns database in body, without errors.', function(done){
-      var apiResponse = {};
       request('http://localhost:3000/api/v2/data', function(error, response, body) {
-        apiResponse.error = error;
-        apiResponse.response = response;
-        apiResponse.body = body;
-        assert(apiResponse.body && !error, 'Missing body or recieved an error response.');
+        assert(body && !error, 'Missing body or recieved an error response.');
         done();
       })
     })
